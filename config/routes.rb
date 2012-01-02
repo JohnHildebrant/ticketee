8887e127b9535d83d167daebbf6fe46518e8fb2d
John Hildebrant <john.hildebrant@gmail.com>
2012-01-02 14:20:41 -0800

    Admins can now set a default state for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,11 @@ Ticketee::Application.routes.draw do
     resources :users do
       resources :permissions
     end
-    resources :states
+    resources :states do
+      member do
+        get :make_default
+      end
+    end
   end
   resources :files
   # The priority is based upon order of creation:
