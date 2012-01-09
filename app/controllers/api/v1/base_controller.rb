c0cd81971f6cb0f001b7bdc0ad15c09e0e7e5ea7
John Hildebrant <john.hildebrant@gmail.com>
2012-01-08 20:49:45 -0800

    Added rate limiting to the V1 API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BaseController < ActionController::Base
   
   before_filter :authenticate_user
+  before_filter :check_rate_limit
   
   respond_to :json, :xml
   
@@ -22,5 +23,14 @@ class Api::V1::BaseController < ActionController::Base
     
     def current_user
       @current_user
+    end
+    
+    def check_rate_limit
+      if @current_user.request_count > 100
+        error = { :error => "Rate limit exceeded." }
+        respond_with(error, :status => 403 )
+      else
+        @current_user.increment!(:request_count)
+      end
     end
 end
