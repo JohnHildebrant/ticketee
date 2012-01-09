c58c00ee58bba994094739a12bdcb750f85d4b83
John Hildebrant <john.hildebrant@gmail.com>
2012-01-08 18:33:36 -0800

    Added API to create projects

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
-  before_filter :authorize_admin!, :except => [:index, :show]
-  before_filter :find_project, :only => [:show, :update, :destroy]
   
   def index
     respond_with(Project.for(current_user))
   end
+  
+  def create
+    project = Project.create(params[:project])
+    if project.valid?
+      respond_with(project, :location => api_v1_project_path(project))
+    else
+      respond_with(project)
+    end
+  end
+  
 end
