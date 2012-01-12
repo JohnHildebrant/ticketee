efd969355203971be88bc95fd760aea63dda4ef5
John Hildebrant <john.hildebrant@wizards.com>
2012-01-12 10:07:30 -0800

    tag permission fixes

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -5,6 +5,7 @@ module Admin::PermissionsHelper
       "create tickets" => "Create Tickets",
       "edit tickets" => "Edit Tickets",
       "delete tickets" => "Delete Tickets",
+      "tag" => "Tag Tickets",
       "change states" => "Change States"
     }
   end
