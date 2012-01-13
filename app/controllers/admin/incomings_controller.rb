79d80fdf5d118f45dedfe4dd26afd66ef5c58e02
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 14:28:40 -0800

    mail_poller update

--- app/controllers/admin/incomings_controller.rb
+++ app/controllers/admin/incomings_controller.rb
@@ -2,5 +2,5 @@ class Admin::IncomingsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   # Being invoked as a POST from the mail_poller
-  Receiver.parse(email)
+  Receiver.parse(params[:email])
 end
