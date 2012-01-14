391f530d62c10d83047884e197bb931d3cc22bab
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 20:31:02 -0800

    tweak

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -13,9 +13,12 @@ class Receiver < ActionMailer::Base
         ticket = project.tickets.find(ticket_id)
         from_exp = /[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/i
         from = from_exp.match(email.from[0])[0]
-        from = from_exp.match(email.from[0])[1] unless from
+        from = from_exp.match(email.from[1])[0] unless from
         user = User.find_by_email(from)
         comment_text = Nokogiri::HTML(comment_text[1].strip).text
+        comment_strip_exp = /^<!--.+-->(.+)$/
+        comment_text = comment_strip_exp.match(comment_text) ? 
+          comment_strip_exp.match(comment_text)[0] : comment_text
         ticket.comments.create(:text => comment_text, :user => user) if user
       end
     end
