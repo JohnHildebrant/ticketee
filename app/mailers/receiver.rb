78767aa54f459f491e5242ed8dda6c10d24f9367
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 18:51:39 -0800

    format issue

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -6,15 +6,15 @@ class Receiver < ActionMailer::Base
   
   def self.parse(email)
     reply_separator = /(.*?)\s?== ADD YOUR REPLY ABOVE THIS LINE ==/m
-    full_text = Nokogiri::HTML(email.body.to_s).text
-    comment_text = reply_separator.match(full_text)
+    comment_text = reply_separator.match(email.body.to_s)
     if comment_text
       to, project_id, ticket_id = email.subject.split("@")[0].split("+")
       if project_id.to_i > 0 && ticket_id.to_i > 0
         project = Project.find(project_id)
         ticket = project.tickets.find(ticket_id)
         user = User.find_by_email(email.from[0])
-        ticket.comments.create(:text => comment_text[1].strip,
+        comment_text = Nokogiri::HTML(comment_text[1].strip).text
+        ticket.comments.create(:text => comment_text,
                                :user => user)
       end
     end
