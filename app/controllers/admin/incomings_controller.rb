ffabc380cabfa74c25b61f374110a2c6e81d2438
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 17:48:22 -0800

    using ActionMailer

--- app/controllers/admin/incomings_controller.rb
+++ app/controllers/admin/incomings_controller.rb
@@ -3,17 +3,6 @@ class Admin::IncomingsController < ApplicationController
   
   def create
     # Being invoked as a POST from the mail_poller
-    mail = Mail.parse(params[:email])
-  
-    reply_separator = /(.*?)\s?== ADD YOUR REPLY ABOVE THIS LINE ==/m
-    comment_text = reply_separator.match(email.body.to_s)
-    if comment_text
-      to, project_id, ticket_id = email.subject.split("@")[0].split("+")
-      project = Project.find(project_id)
-      ticket = project.tickets.find(ticket_id)
-      user = User.find_by_email(email.from[0])
-      ticket.comments.create(:text => comment_text[1].strip,
-                             :user => user)
-    end
+    Receiver.parse(params[:email])
   end                
 end
