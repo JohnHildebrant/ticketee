9afdf5e4e65b0d547316cb66cc1ccdfa4f95f139
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 18:18:27 -0800

    fixed id bug

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -6,11 +6,13 @@ class Receiver < ActionMailer::Base
     comment_text = reply_separator.match(email.body.to_s)
     if comment_text
       to, project_id, ticket_id = email.subject.split("@")[0].split("+")
-      project = Project.find(project_id)
-      ticket = project.tickets.find(ticket_id)
-      user = User.find_by_email(email.from[0])
-      ticket.comments.create(:text => comment_text[1].strip,
-                             :user => user)
+      if project_id.to_i > 0 && ticket_id.to_i > 0
+        project = Project.find(project_id)
+        ticket = project.tickets.find(ticket_id)
+        user = User.find_by_email(email.from[0])
+        ticket.comments.create(:text => comment_text[1].strip,
+                               :user => user)
+      end
     end
   end
 end
