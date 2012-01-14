a9c116ead8d695eae7b445bb0a4a1abfb46e368a
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 19:10:23 -0800

    email churn

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -1,20 +1,16 @@
 class Receiver < ActionMailer::Base
-  require 'rubygems'
-  require 'nokogiri'
-  
   default from: "opsmailer@wizards.com"
   
   def self.parse(email)
     reply_separator = /(.*?)\s?== ADD YOUR REPLY ABOVE THIS LINE ==/m
-    comment_text = reply_separator.match(email.body.to_s)
+    comment_text = reply_separator.match(email.body.decoded)
     if comment_text
       to, project_id, ticket_id = email.subject.split("@")[0].split("+")
       if project_id.to_i > 0 && ticket_id.to_i > 0
         project = Project.find(project_id)
         ticket = project.tickets.find(ticket_id)
-        user = User.find_by_email(email.from[0])
-        comment_text = Nokogiri::HTML(comment_text[1].strip).text
-        ticket.comments.create(:text => comment_text,
+        user = User.find_by_email(email.envelope.from)
+        ticket.comments.create(:text => comment_text[1].strip,
                                :user => user)
       end
     end
