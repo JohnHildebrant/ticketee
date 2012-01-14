57350ce96f42eb51c54e7dcf91f4749145742ac5
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 22:44:59 -0800

    testing

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -12,11 +12,11 @@ class Receiver < ActionMailer::Base
         project = Project.find(project_id)
         ticket = project.tickets.find(ticket_id)
         user = User.find_by_email(email.from[0].downcase)
-        comment_text = Nokogiri::HTML(comment_text[1].strip).text
-        comment_text.bomb
-        comment_strip_exp = /^<!--.+-->;(.+)$/m
-        comment_text = comment_strip_exp.match(comment_text) ? 
-          comment_strip_exp.match(comment_text)[0] : comment_text
+        comment_text = comment_text[1].strip
+        comment_text = Nokogiri::HTML(comment_text).text
+        comment_strip_exp = /^&lt;!--.+--&gt;(.+)$/m
+        match_text = comment_strip_exp.match(comment_text)[0]
+        match_text.bomb
         ticket.comments.create(:text => comment_text, :user => user)
       end
     end
