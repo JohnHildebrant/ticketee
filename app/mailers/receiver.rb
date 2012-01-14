1f8ac501d3800ce335ae536152fca4277bbe282f
John Hildebrant <john.hildebrant@wizards.com>
2012-01-14 11:56:34 -0800

    debug

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -9,15 +9,15 @@ class Receiver < ActionMailer::Base
     log = Logger.new(logfile)
     reply_separator = /(.*?)\s?From:\s+WOTC OPSmailer/m
     body = email.body.decoded
-    log.info "Email body = " + body
     body = Nokogiri::HTML(body).text
     log.info "Email body = " + body
     comment_text = reply_separator.match(body)
-    log.info "Comment = " + comment_text[1].strip if comment_text
-    comment_text.bomb
-    reply_separator = /(.*?)\s?== ADD YOUR REPLY ABOVE THIS LINE ==/m
-    comment_text = reply_separator.match(email.body.decoded) unless comment_text
-    
+    if comment_text
+      comment_text = comment_text[1].strip 
+    else
+      reply_separator = /(.*?)\s?== ADD YOUR REPLY ABOVE THIS LINE ==/m
+      comment_text = reply_separator.match(email.body.decoded)
+    end
     if comment_text
       to, project_id, ticket_id = email.subject.split("@")[0].split("+")
       if project_id.to_i > 0 && ticket_id.to_i > 0
@@ -25,7 +25,6 @@ class Receiver < ActionMailer::Base
         ticket = project.tickets.find(ticket_id)
         user = User.find_by_email(email.from[0].downcase)
         comment_text = comment_text[1].strip
-        comment_text = Nokogiri::HTML(comment_text).text
         comment_strip_exp = /^<!--.+-->(.+)$/m
         match_text = comment_strip_exp.match(comment_text)
         log.info "Comment = " + comment_text
