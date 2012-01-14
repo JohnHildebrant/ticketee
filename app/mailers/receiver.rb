1ce8cd502f2676a1b0b4f8ed6ddd7b6a362714c6
John Hildebrant <john.hildebrant@wizards.com>
2012-01-13 18:41:00 -0800

    converting email html to text

--- app/mailers/receiver.rb
+++ app/mailers/receiver.rb
@@ -1,9 +1,13 @@
 class Receiver < ActionMailer::Base
+  require 'rubygems'
+  require 'nokogiri'
+  
   default from: "opsmailer@wizards.com"
   
   def self.parse(email)
     reply_separator = /(.*?)\s?== ADD YOUR REPLY ABOVE THIS LINE ==/m
-    comment_text = reply_separator.match(email.body.to_s)
+    full_text = Nokogiri::HTML(email.body.to_s).text
+    comment_text = reply_separator.match(full_text)
     if comment_text
       to, project_id, ticket_id = email.subject.split("@")[0].split("+")
       if project_id.to_i > 0 && ticket_id.to_i > 0
