e7c4e9d54629f1c6b57f3fc3b9e9b2c3be87e8b4
John Hildebrant <john.hildebrant@wizards.com>
2012-01-26 17:51:09 -0800

    gsub \r\n to \n

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,12 @@ Ticketee::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+  
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
