b4fa746d146d7beba2413dcbadad79a30992b722
John Hildebrant <john.hildebrant@wizards.com>
2012-02-02 01:46:37 -0800

    post to tickets controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 require 'htmlentities'
 
 class CommentsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_filter :authenticate_user!
   before_filter :find_ticket
 
